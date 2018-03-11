6058de6aa810f26b1452ae5475da3bf54a54ffd7
kenny <ekeneeze3@gmail.com>
2018-03-12 00:24:21 +0100

    Update Signature Provider

--- app/src/main/java/com/example/ekene/cloudinarsample/MainActivity.java
+++ app/src/main/java/com/example/ekene/cloudinarsample/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.ekene.cloudinarsample;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.os.Bundle;
+import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.cloudinary.Transformation;
 import com.cloudinary.android.MediaManager;
@@ -18,105 +25,159 @@ import com.cloudinary.android.callback.ErrorInfo;
 import com.cloudinary.android.callback.UploadCallback;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<ListClass> listClassItems;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
     private ImageView imageView;
     private static final int INTENT_REQUEST_CODE = 222;
-    FloatingActionButton fab;
+    private Button postBtn;
+    String returnedTitle ="";
+    String returnedPrice = "";
+    EditText edt_price;
+    private EditText edt_title;
+    private LinearLayout postLayout, displayLayout;
+    private FloatingActionButton fab;
+    private String imageUri;
+    private String image_title;
+    private String image_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        imageView = findViewById(R.id.imageView);
-
+        edt_price = findViewById(R.id.edt_price);
+        edt_title = findViewById(R.id.edt_title);
+        image_price = edt_price.getText().toString();
+        image_title = edt_title.getText().toString();
+        recyclerView = findViewById(R.id.recyclerviewpost);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        postLayout = findViewById(R.id.postLayout);
+        displayLayout = findViewById(R.id.displayLayout);
+        displayLayout.setVisibility(View.INVISIBLE);
         fab = findViewById(R.id.fab);
-
-        MediaManager.init(this);
-
+        MediaManager.init(this, new BackendServerSignatureProvider());
         fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postLayout.setVisibility(View.VISIBLE);
+            }
+        });
+        listClassItems = new ArrayList<>();
+        imageView = findViewById(R.id.imageBtn);
+        postBtn = findViewById(R.id.postBtn);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 chooseImageFromDevice();
             }
         });
-
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayLayout.setVisibility(View.VISIBLE);
+            }
+        });
     }
-
     public void chooseImageFromDevice() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("image/*");
         startActivityForResult(intent, INTENT_REQUEST_CODE);
     }
+    private void showSnackBar(String message) {
+        Snackbar.make(fab, message, Snackbar.LENGTH_LONG).show();
+    }
     public String getUrlForMaxWidth(String imageId) {
         int width = Utils.getScreenWidth(this);
         return MediaManager.get().getCloudinary().url().transformation(new Transformation().width(width)).generate(imageId);
     }
-
-    private void showSnackBar(String message) {
-        Snackbar.make(fab, message, Snackbar.LENGTH_LONG).show();
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == INTENT_REQUEST_CODE && data != null && data.getData() != null) {
-            MediaManager.get().upload(data.getData()).callback(new UploadCallback() {
+        if (resultCode == RESULT_OK && requestCode == INTENT_REQUEST_CODE && data != null &&
+                data.getData() != null) {
+            if (resultCode == RESULT_OK && requestCode == INTENT_REQUEST_CODE && data != null &&
+                    data.getData() != null) {
+                image_price = edt_price.getText().toString();
+                image_title = edt_title.getText().toString();
 
-                @Override
-                public void onStart(String requestId) {
-                    showSnackBar("Upload started...");
+                final Map<String,String> map = new HashMap<>();
+                map.put("title", image_title);
+                map.put("price", image_price);
+                final String context = map.toString();
+
+                if (!TextUtils.isEmpty(image_price) && !TextUtils.isEmpty(image_title)){
+                    MediaManager.get().upload(data.getData())
+                            .option("folder", "images/")
+                            .option("tags", "food")
+                            .option("context", context)
+                            .callback(new UploadCallback() {
+                                @Override
+                                public void onStart(String requestId) {
+                                    showSnackBar("Upload started...");
+                                }
+                                @Override
+                                public void onProgress(String requestId, long bytes,
+                                                       long totalBytes) {
+                                }
+                                @Override
+                                public void onSuccess(String requestId, Map resultData) {
+                                    showSnackBar("Upload complete!");
+                                    Picasso.with(MainActivity.this).load(getUrlForMaxWidth(resultData
+                                            .get("public_id").toString())).into(imageView);
+
+                                    //Reading the context response
+                                    Map<String, String> contextResponseData  = (HashMap)resultData.get("context");
+                                    Map.Entry<String,String> Mapvalue = contextResponseData.entrySet().iterator().next();
+
+                                    //get the map key
+                                    String key  = Mapvalue.getKey(); // returns "custom"
+
+                                    //get the map value i.e title and price
+//                                    String value = Mapvalue.getValue(); //throws java.lang.ClassCastException: java.util.HashMap cannot be cast to java.lang.String
+
+                                    //returnedTitle = value.get("title");
+                                    //returnedPrice = value.get("price");
+
+                                    ListClass Lclass = new ListClass(getUrlForMaxWidth(resultData
+                                            .get("public_id").toString()),
+                                            returnedTitle, returnedPrice);
+                                    listClassItems.add(Lclass);
+                                    adapter = new MyAdapter(listClassItems, getApplicationContext());
+                                    recyclerView.setAdapter(adapter);
+                                    Log.d("RESPONSE", resultData.toString());
+
+                                }
+                                @Override
+                                public void onError(String requestId, ErrorInfo error) {
+                                    showSnackBar("Upload error: " + error.getDescription());
+                                }
+                                @Override
+                                public void onReschedule(String requestId, ErrorInfo error) {
+                                    showSnackBar("Upload rescheduled.");
+                                }
+                            }).dispatch();
+                }else {
+                    showSnackBar("Empty fields...");
                 }
 
-                @Override
-                public void onProgress(String requestId, long bytes, long totalBytes) {
-                }
-
-                @Override
-                public void onSuccess(String requestId, Map resultData) {
-                    Picasso.with(MainActivity.this).load(getUrlForMaxWidth(resultData.get("public_id").toString())).into(imageView);
-                    showSnackBar("Upload complete!");
-                }
-
-                @Override
-                public void onError(String requestId, ErrorInfo error) {
-                    showSnackBar("Upload error: " + error.getDescription());
-                }
-
-                @Override
-                public void onReschedule(String requestId, ErrorInfo error) {
-                    showSnackBar("Upload rescheduled.");
-                }
-            }).dispatch();
-        }}
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            }
         }
 
-        return super.onOptionsItemSelected(item);
     }
-
 }
--- app/src/main/java/com/example/ekene/cloudinarsample/Utils.java
+++ app/src/main/java/com/example/ekene/cloudinarsample/Utils.java
@@ -4,10 +4,6 @@ import android.content.Context;
 import android.graphics.Point;
 import android.view.WindowManager;
 
-/**
- * Created by EKENE on 3/1/2018.
- */
-
 public class Utils {
     public static int getScreenWidth(Context context) {
         WindowManager window = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
